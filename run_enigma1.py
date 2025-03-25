cae08909b86e00eaa875377690f8263f2dbb7c8f
She Zhang <sercharles19920620@gmail.com>
2025-03-24 18:26:39 -0700

    add comments for run_enigma1.py

--- run_enigma1.py
+++ run_enigma1.py
@@ -1,4 +1,3 @@
-from utils import *
 from utils_enigma import *
 import json
 
@@ -12,20 +11,26 @@ with open("words_dictionary.json", "r") as file:
     wordlist = json.load(file)
 wordlist = set(wordlist.keys())
 
+# build scrambler
 S = {k: v for k, v in zip(scramble, ALPHABET)}
+# build reflector
 R = {k: v for k, v in zip(reflect, ALPHABET)}
 
 rot_sgn = -1
 for init_n in range(26):
+    # get scramblers for each rotation
     Ss = [rotate_disc(S, int(rot_sgn * (n + init_n))) for n in range(len(cipher))]
+    # get inverse scramblers for each rotation
     Ss_inv = [get_inv_map(s) for s in Ss]
-
+    # build enigma prototype, transformation: S^-1 R S
     enig = lambda c, i: Ss_inv[i][ R[ Ss[i][c] ] ]
 
+    # decode using the prototype
     plain = ""
     for i, c in enumerate(cipher):
         plain += enig(c, i)
     
+    # if the plaintext means something, label it
     if plain.lower() in wordlist:
         plain += ' *'
 
